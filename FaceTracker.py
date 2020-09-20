e825c4339a0ae7f05ca9cc33f885bb102d322302
Ewan Drever-Smith <eeyerd@nottingham.ac.uk>
2020-09-20 01:20:57 +0100

    Added comments

--- FaceTracker.py
+++ FaceTracker.py
@@ -8,11 +8,13 @@ faceCascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 yawServo = Servo(17)
 
+#PID Controller setup
 P, I, D = 0.5, 0, 0
 integral = 0
 differential = 0
 previousError = 0
 
+#Camera setup
 width, height = 320, 240
 camera = PiCamera()
 camera.resolution = (width, height)
@@ -20,17 +22,18 @@ camera.framerate = 30
 rawCapture = PiRGBArray(camera, size = (width, height))
 time.sleep(0.1)
 
+
 for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = True):
     image = frame.array
     frame = cv.flip(image, 1)
 
-    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #convert to greyscale
 
-    faces = faceCascade.detectMultiScale(grayFrame, 1.3, 5)
+    faces = faceCascade.detectMultiScale(grayFrame, 1.3, 5) #Find all faces in frame using haar cascade
 
     for(x,y,w,h) in faces:
         faceCentroidX = x + w / 2
-        error = width / 2 - faceCentroidX
+        error = width / 2 - faceCentroidX #use distance of face from center of frame as error
         integral += error
         differential = previousError - error
         previousError = error
@@ -38,12 +41,12 @@ for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_por
         adjustmentValue = P * error + I * integral + D * differential
         yawServo.changeAngle(-adjustmentValue)
 
-        frame = cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
+        frame = cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4) #highlight face with rectangle
         
         break #only consider the first face
     
     cv.imshow('ComputerView',frame) #display image
-    key = cv2.waitKey(1) &amp; 0xFF
+    key = cv.waitKey(1) &amp; 0xFF
     rawCapture.truncate(0)
     if key == ord("q"):
         break
--- Servo.py
+++ Servo.py
@@ -1,39 +1,41 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 
+#Servo specific values
 upperDC = 12
 lowerDC = 2
 rangeDC = upperDC - lowerDC
 maxAngle = 180
 
 class Servo():
-    def __init__(self, servoPin):
-        GPIO.setmode(GPIO.BCM)
+    def __init__(self, servoPin):   #setup
+        GPIO.setmode(GPIO.BCM) #notice BCM pin labels
         GPIO.setwarnings(False)
         GPIO.setup(servoPin,GPIO.OUT)
+        
         self.servoPWM = GPIO.PWM(servoPin, 50)
         self.servoPWM.start(rangeDC / 2 + lowerDC)
         self.currentAngle = maxAngle / 2
         sleep(1)
         self.servoPWM.ChangeDutyCylce(0)
         
-    def __del__(self):
+    def __del__(self): #destructor
         self.servoPWM.stop()
         GPIO.cleanup()
         
-    def setDC(self, dc):
+    def setDC(self, dc): #Safely sets the duty cycle
         self.servoPWM.ChangeDutyCycle(dc)
         sleep(0.05)
         self.servoPWM.ChangeDutyCycle(0)
         self.currentAngle = (dc - lowerDC) / rangeDC * maxAngle
 
-    def setAngle(self, angle):
+    def setAngle(self, angle): #Calculate angles corresponding duty cycle
         self.servoPWM.ChangeDutyCycle(angle / maxAngle * rangeDC + lowerDC)
         sleep(0.05)
         self.servoPWM.ChangeDutyCycle(0)
         self.currentAngle = angle
 
-    def changeAngle(self, delta):
+    def changeAngle(self, delta): #increments/decrements angle of servo
         self.currentAngle += delta
         self.setAngle(self.currentAngle)
         
